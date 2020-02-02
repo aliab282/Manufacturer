2c7f4e83f7ce22597371adfbac6caf245db75eac
Ali Abbasi <aabbasi541@gmail.com>
2020-02-02 18:33:53 +0330

    Initial commit

---
+++ ManufacturerUtils/src/main/java/com/example/manufacturerutils/manufacturerutils.java
@@ -0,0 +1,52 @@
+package com.example.manufacturerutils;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+public class manufacturerutils {
+    /**
+     * @param context      needed to interact with drawables from resource id
+     * @param manufacturer short format name of the manufacturer.
+     * @return drawable of the manufacturer, or null if cannot find any.
+     */
+    public static Drawable getLogoDrawable(Context context, String manufacturer) {
+        Integer resId;
+        switch (manufacturer) {
+            case "Samsung":
+                resId = R.drawable.logo_samsung;
+                break;
+            case "Sony":
+                resId = R.drawable.logo_sony;
+                break;
+            case "Acer":
+                resId = R.drawable.logo_acer;
+                break;
+            case "Asus":
+                resId = R.drawable.logo_asus;
+                break;
+            case "Google":
+                resId = R.drawable.logo_google;
+                break;
+            case "HTC":
+                resId = R.drawable.logo_htc;
+                break;
+            case "Huawei":
+                resId = R.drawable.logo_huawei;
+                break;
+            case "LG":
+                resId = R.drawable.logo_lg;
+                break;
+            case "Motorola":
+                resId = R.drawable.logo_motorola;
+                break;
+            default:
+                resId = null;
+        }
+
+        if (resId == null) {
+            return null;
+        } else {
+            return context.getResources().getDrawable(resId);
+        }
+    }
+}
